db3cef74486c8092c275719ab4e930dc5ee6b1c3
Kenneth Chen <unusualkid@gmail.com>
2020-09-02 01:29:00 +0800

    Remove unused boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
-import "./App.css";
+import React, { Component } from "react"
+import NavBar from "./components/navbar"
+import Counters from "./components/counters"
 
 class App extends Component {
   state = {
@@ -11,47 +10,47 @@ class App extends Component {
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
-  };
+  }
 
   constructor(props) {
-    super(props);
-    console.log("App - Constructor");
+    super(props)
+    console.log("App - Constructor")
     // this.state = this.props.something;
   }
 
   componentDidMount() {
     //Ajax call
-    console.log("App - Mounted");
+    console.log("App - Mounted")
   }
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter }
+    counters[index].value++
+    this.setState({ counters })
+  }
 
   handleReset = () => {
-    const counters = this.state.counters.map((counter) => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters });
-  };
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0
+      return counter
+    })
+    this.setState({ counters })
+  }
 
-  handleDelete = (counterId) => {
+  handleDelete = counterId => {
     const counters = this.state.counters.filter(
-      (counter) => counter.id !== counterId
-    );
-    this.setState({ counters });
-  };
+      counter => counter.id !== counterId
+    )
+    this.setState({ counters })
+  }
   render() {
-    console.log("App - Rendered");
+    console.log("App - Rendered")
     return (
       <React.Fragment>
         <NavBar
-          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
         />
         <main className="container">
           <Counters
@@ -62,8 +61,8 @@ class App extends Component {
           />
         </main>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
